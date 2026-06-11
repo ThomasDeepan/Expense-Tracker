e8f9b325b50df3102c5b356ae652eceefe39c1c2
ThomasDeepan <thomasdeepan05@gmail.com>
2026-06-11 21:05:59 +0530

    backend deployed on render

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://tracker-tom.vercel.app"], // Tell the server to only trust your React app
+    origin: ["https://tom-tracker.vercel.app"], // Tell the server to only trust your React app
   }),
 );
 
