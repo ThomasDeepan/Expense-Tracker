dbbc34c09377ff4d4808ed8a6408f5b3869ea735
ThomasDeepan <thomasdeepan05@gmail.com>
2026-06-11 20:02:12 +0530

    backend deployed on render

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,38 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // Tell the server to only trust your React app
+    origin: ["https://tracker-tom.vercel.app"], // Tell the server to only trust your React app
   }),
 );
 
 // Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ DB Error:", err));
+let isConnected = false;
+app.use(async (req, res, next) => {
+  if (isConnected) return next();
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("✅ MongoDB Connected");
+    next();
+  } catch (err) {
+    console.log("❌ DB Error:", err);
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
 
 // Routes (We will create these next)
 app.use("/api/v1", require("./routes/transaction"));
 
+// Base route to check if API is alive
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Tracker.Tom API is running smoothly!",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+module.exports = app;
